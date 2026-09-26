0098ef28b90be29ba8d23e0faac7e048e9bc44ec
zdegroot <degroot@chapman.edu>
2026-09-25 17:03:16 -0700

    Added test c# file/first push to zach branch

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("this is a test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
